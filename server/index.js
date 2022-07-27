c8b994aa93475e64157e5ceb2bf6d5a1d7635bce
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-07-27 17:52:02 +0300

    w27.07.2022

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import cors from "cors"
 import express from "express"
 import fs from "fs"
 import path from "path"
+import os from "os"
 
 const __dirname = path.resolve()
 const PORT = 3001
@@ -40,4 +41,38 @@ app.post("/set-db", (req, res) => {
     })
 })
 
-app.listen(PORT, () => { console.log("сервер запущен"); })
+app.listen(PORT, () => {
+    console.log("сервер запущен");
+    console.log(os.networkInterfaces()['Беспроводная сеть 2'][1].address);
+    
+    fs.readFile("./template/main.0f71035c.js", { encoding: 'utf-8' }, (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            let file = data
+            file = file.replaceAll('localhost', os.networkInterfaces()['Беспроводная сеть 2'][1].address)
+            fs.writeFile('./static/static/js/main.0f71035c.js', file, (error) => {
+                if (error) {
+                    console.log(error);
+                } else{
+                    console.log('file has been change');
+                }    
+            })
+        }
+    })
+    fs.readFile("./template/main.0f71035c.js.map", { encoding: 'utf-8' }, (error, data) => {
+        if (error) {
+            console.log(error);
+        } else {
+            let file = data
+            file = file.replaceAll('localhost', os.networkInterfaces()['Беспроводная сеть 2'][1].address)
+            fs.writeFile('./static/static/js/main.0f71035c.js.map', file, (error) => {
+                if (error) {
+                    console.log(error);
+                } else{
+                    console.log('file has been change');
+                }    
+            })
+        }
+    })
+})

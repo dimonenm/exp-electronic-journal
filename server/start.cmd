f5e1594204217aaec2698fb8fdf39230bfc0ac3b
exp <arzuakov@yahoo.com>
2022-07-28 23:30:53 +0300

    h28.07.2022

---
+++ server/start.cmd
@@ -0,0 +1,5 @@
+%LOCALAPPDATA%\Yandex\YandexBrowser\Application\browser http://localhost:4000
+cd /.
+cd c:\OpenServer\domains\localhost\react_lab\exp-photo-table\server\
+@REM pm2 start index.js
+npm start

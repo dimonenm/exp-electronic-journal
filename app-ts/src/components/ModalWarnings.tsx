fdb3a1444293844534a73eac4fe9163bea0c1fff
Russs <russs.khalilov@gmail.com>
2023-12-31 12:50:05 +0300

    h31.12.2023

--- app-ts/src/components/ModalWarnings.tsx
+++ app-ts/src/components/ModalWarnings.tsx
@@ -2,15 +2,25 @@ import React, { FC } from 'react';
 import Exp from '../entities/Exp';
 import WarningsText from './WarningsText';
 import WarningsTitle from './WarningsTitle';
+import { IModal } from '../types/types';
 
 interface ModalWarningsProps {
-  dbExps: Exp[]
+  dbExps: Exp[],
+  setModal: React.Dispatch<IModal>
 }
 
-const ModalWarnings: FC<ModalWarningsProps> = ({ dbExps }) => {
+const ModalWarnings: FC<ModalWarningsProps> = ({ dbExps, setModal }) => {
 
   let expWarningsComponentsArr: React.ReactNode[] = []
   let petitionsWarningsComponentsArr: React.ReactNode[] = []
+
+  function updateClickHendler(number: string | null) {
+    setModal({
+      type: 'update', idOfExp: number
+    })
+  }
+
+
 
   dbExps.forEach((item) => {
     if (item.getResult() === '' || item.getResult() === 'не указано') {
@@ -21,12 +31,12 @@ const ModalWarnings: FC<ModalWarningsProps> = ({ dbExps }) => {
         if (daysCount <= 5 && daysCount >= 0) {
           const dayOfWeek = daysCount === 0 ? 'дней' : daysCount === 5 ? 'дней' : daysCount === 1 ? 'день' : 'дня'
           const text = `№${item.getId()} - ${daysCount} ${dayOfWeek} до окончания экспертизы`
-          expWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} />)
+          expWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} number={item.getId()} updateClickHendler={updateClickHendler} />)
         }
         if (daysCount < 0) {
           const dayOfWeek = daysCount < -5 ? 'дней' : daysCount === -1 ? 'день' : 'дня'
           const text = `№${item.getId()} - ${daysCount * -1} ${dayOfWeek} после окончания экспертизы`
-          expWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} />)
+          expWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} number={item.getId()} updateClickHendler={updateClickHendler} />)
         }
       }
       if (item.getDatePetitionStart() !== '' && item.getDatePetitionEnd() === '' && item.getValueOfProlongation() === '') {
@@ -36,12 +46,12 @@ const ModalWarnings: FC<ModalWarningsProps> = ({ dbExps }) => {
         if (daysCount <= 3 && daysCount >= 0) {
           const dayOfWeek = daysCount === 0 ? 'дней' : daysCount === 1 ? 'день' : 'дня'
           const text = `№${item.getId()} - ${daysCount} ${dayOfWeek} до окончания ходатайства`
-          petitionsWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} />)
+          petitionsWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} number={item.getId()} updateClickHendler={updateClickHendler} />)
         }
         if (daysCount < 0) {
           const dayOfWeek = daysCount < -5 ? 'дней' : daysCount === -1 ? 'день' : 'дня'
           const text = `№${item.getId()} - ${daysCount * -1} ${dayOfWeek} после окончания ходатайства`
-          petitionsWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} />)
+          petitionsWarningsComponentsArr.push(<WarningsText key={item.getId()} text={text} number={item.getId()} updateClickHendler={updateClickHendler} />)
         }
       }
     }
